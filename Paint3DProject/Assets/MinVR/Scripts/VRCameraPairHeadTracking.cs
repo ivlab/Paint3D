badab0d8e6638c68229cd5528d2aa018bf79ecf7
Jung Nam <namxx054@umn.edu>
2015-12-04 15:24:01 -0600

    Buttons and Multi-User Head-tracking
    Add button and multi-users head-tracking functionalities.

--- Paint3DProject/Assets/MinVR/Scripts/VRCameraPairHeadTracking.cs
+++ Paint3DProject/Assets/MinVR/Scripts/VRCameraPairHeadTracking.cs
@@ -2,33 +2,38 @@
 using System.Collections;
 
 
-namespace MinVR {
-
-	public class VRCameraPairHeadTracking : MonoBehaviour {
+namespace MinVR
+{
+/*
+	public class VRCameraPairHeadTracking : MonoBehaviour
+	{
 
 		public string headTrackingEvent = "Head_Move";
 		public string matrix4x4DataField = "Transform";
 
 
 		// Use this for initialization
-		void Start () {
+		void Start ()
+		{
 			VRMain.VREventHandler += OnVREvent;
 		}
 		
 		// Update is called once per frame
-		void Update () {
+		void Update ()
+		{
 		
 		}
 
-		void OnVREvent(VREvent e) {
+		void OnVREvent (VREvent e)
+		{
 			if (e.Name == headTrackingEvent) {		
-				Matrix4x4 m = VRConvert.ToMatrix4x4(e.DataIndex.GetValueAsDoubleArray(matrix4x4DataField));
-				transform.position = m.GetTranslation();
-				transform.rotation = m.GetRotation();
+				Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray (matrix4x4DataField));
+				transform.position = m.GetTranslation ();
+				transform.rotation = m.GetRotation ();
 				//Debug.Log("Head Position = " + m.GetTranslation().ToString());
 				//Debug.Log("Head Rotation = " + m.GetRotation().ToString());
 			}
 		}
-	}
+	}*/
 
 } // namespace MinVR
--- Paint3DProject/Assets/Paint3D/Scripts/Paint3DMain.cs
+++ Paint3DProject/Assets/Paint3D/Scripts/Paint3DMain.cs
@@ -8,40 +8,62 @@ using MinVR;
  * It also uses the FixedUpdate() callback to create new '3D paint strokes' when the brush 
  * button is pressed.
  */
-public class Paint3DMain : MonoBehaviour {
+public class Paint3DMain : MonoBehaviour
+{
 
 	[Tooltip("This GameObject will move around with the tracker attached to the Brush.")]
-	public GameObject brushCursor;
+	public GameObject
+		brushCursor;
 	[Tooltip("This GameObject will move around with the tracker attached to the Hand.")]
-	public GameObject handCursor;
+	public GameObject
+		handCursor;
+
+	public GameObject
+		headObj;
+	
+	private bool startDrawing = false;
+
+	// determine user: TODO: send signal to server and get feedback from server to determine the user id
+	public bool user_id = true;	// default: user 1
 
 
-	void Start () {
+	void Start ()
+	{
 		VRMain.VREventHandler += OnVREvent;
 	}
 
 
-	void FixedUpdate () {
+	void FixedUpdate ()
+	{
 
 		// If brush button is down, then add to the painting
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButton (0)) {
 			// TODO: This creates a new painted object each frame.  Instead, for most brush types, 
 			// we probably want to create a new BrushStroke the first time the brush button is 
 			// pressed and then add to it each frame until the button is released.
-			GameObject paintedObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			paintedObj.transform.parent = GameObject.Find("Painting").transform;
+			GameObject paintedObj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			paintedObj.transform.parent = GameObject.Find ("Painting").transform;
 			paintedObj.transform.position = brushCursor.transform.position;
 			paintedObj.transform.rotation = brushCursor.transform.rotation;
-			paintedObj.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+			paintedObj.transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
 		}
 
 		// If the hand button is down, then grab on to the painting and move it about
-		if (Input.GetMouseButton(1)) {
+		if (Input.GetMouseButton (1)) {
 			Vector3 posChange = handPos - lastHandPos;
-			Quaternion rotChange = Quaternion.Inverse(handRot) * lastHandRot;
-			GameObject painting = GameObject.Find("Painting");
+			Quaternion rotChange = Quaternion.Inverse (handRot) * lastHandRot;
+			GameObject painting = GameObject.Find ("Painting");
 			painting.transform.position += posChange;
 			painting.transform.rotation *= rotChange;
+		}
+		
+		//
+		if (startDrawing) {
+			GameObject paintedObj = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			paintedObj.transform.parent = GameObject.Find ("Painting").transform;
+			paintedObj.transform.position = brushCursor.transform.position;
+			paintedObj.transform.rotation = brushCursor.transform.rotation;
+			paintedObj.transform.localScale = new Vector3 (0.3f, 0.3f, 0.3f);
 		}
 
 
@@ -54,31 +76,72 @@ public class Paint3DMain : MonoBehaviour {
 	// This function gets called every time a new VREvent is generated.  Typically, VREvents will come
 	// from the MinVR server, which polls trackers, buttons, and other input devices for input.  When
 	// debugging on your laptop, you can also generate 'fake' VREvents using the VRMain script.
-	void OnVREvent(VREvent e) {
-		if (e.Name == "Brush_Move") {		
-			Matrix4x4 m = VRConvert.ToMatrix4x4(e.DataIndex.GetValueAsDoubleArray("Transform"));
-			brushPos = m.GetTranslation();
-			brushRot = m.GetRotation();
+	void OnVREvent (VREvent e)
+	{
+		if (e.Name == "Brush_Move") {	// for faster user 2 	
+			Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray ("Transform"));
+			brushPos = m.GetTranslation ();
+			brushRot = m.GetRotation ();
 			brushCursor.transform.position = brushPos;
 			brushCursor.transform.rotation = brushRot;
-		} 
-		else if (e.Name == "Hand_Move") {
-			Matrix4x4 m = VRConvert.ToMatrix4x4(e.DataIndex.GetValueAsDoubleArray("Transform"));
-			handPos = m.GetTranslation();
-			handRot = m.GetRotation();
+		} else if (e.Name == "Hand_Move") {
+			Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray ("Transform"));
+			handPos = m.GetTranslation ();
+			handRot = m.GetRotation ();
 			handCursor.transform.position = handPos;
 			handCursor.transform.rotation = handRot;
-		}
-		else if (e.Name == "Head_Move") {
-			Matrix4x4 m = VRConvert.ToMatrix4x4(e.DataIndex.GetValueAsDoubleArray("Transform"));
-			headPos = m.GetTranslation();
-			handRot = m.GetRotation();
-		}
-		else if (e.Name == "Mouse_Down") {
-			Debug.Log("mouse down");
-		}
-		else if (e.Name == "Mouse_Up") {
-			Debug.Log("mouse up");
+		} else if (e.Name == "user_header_1" && user_id) { // for cave painting system user 1
+			Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray ("Transform"));
+			headPos = m.GetTranslation ();
+			headRot = m.GetRotation ();
+			headObj.transform.position = headPos;
+			headObj.transform.rotation = headRot;
+		} else if (e.Name == "user_header_2" && !user_id) { // for cave painting system user 2
+			Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray ("Transform"));
+			handPos = m.GetTranslation ();
+			handRot = m.GetRotation ();
+			headObj.transform.position = headPos;
+			headObj.transform.rotation = headRot;
+		} else if (e.Name == "Head_Move") {
+			Matrix4x4 m = VRConvert.ToMatrix4x4 (e.DataIndex.GetValueAsDoubleArray ("Transform"));
+			headPos = m.GetTranslation ();
+			handRot = m.GetRotation ();
+		} else if (e.Name == "Mouse_Down") {	// TODO : useless for now
+			Debug.Log ("mouse down");
+		} else if (e.Name == "Mouse_Up") {
+			Debug.Log ("mouse up");
+		} else if (e.Name == "stylus1_btn0_down") {        // user 2 start to draw
+			startDrawing = true;
+			Debug.Log ("Red User 2 drawing down");
+
+		} else if (e.Name == "stylus1_btn0_up") {
+			startDrawing = false;
+			Debug.Log ("Red User 2 drawing up");
+                
+		} else if (e.Name == "stylus1_btn1_down") {    // controll what user 2 see
+			// TODO : manipulate User 2 camera
+			Debug.Log ("Red User 2 control down");
+                
+		} else if (e.Name == "stylus1_btn1_up") {
+			// TODO : .... I don't know for now
+			Debug.Log ("Red User 2 control up");
+                
+		} else if (e.Name == "stylus1_btn1_down") {    // user 1 start to draw
+			startDrawing = true;
+			Debug.Log ("Red User 1 control down");
+               
+		} else if (e.Name == "stylus1_btn1_up") {
+			startDrawing = false;
+			Debug.Log ("Red User 1 control up");
+               
+		} else if (e.Name == "stylus1_btn1_down") {    // controll what user 1 see
+			// TODO : manipulate User 1 camera
+			Debug.Log ("Red User 1 control down");
+                
+		} else if (e.Name == "stylus1_btn1_up") {    
+			// TODO : .... I don't know for now
+			Debug.Log ("Red User 1 control up");
+                
 		}
 	}
 
