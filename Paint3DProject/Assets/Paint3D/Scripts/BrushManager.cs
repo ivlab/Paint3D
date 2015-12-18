9068d871c13c1b51596114bb8d90d67b16d149ac
Bryan C. Jones <jone0751@umn.edu>
2015-12-17 18:36:08 -0600

    Added Brush Manager

---
+++ Paint3DProject/Assets/Paint3D/Scripts/BrushManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BrushManager : MonoBehaviour {
+    //                  name,     options {  name, type } 
+    public Dictionary<string, Dictionary<string, System.Type>> availableBrushes;
+
+    public IBrush CreateBrush(string name, Dictionary<string, object> options)
+    {
+        // try and create the brush
+        // if unable to instantiat, return LineBrush by default 
+        IBrush o = (ScriptableObject.CreateInstance(name) as IBrush) ?? ScriptableObject.CreateInstance<LineBrush>();
+        return o;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }    //
+	
+}
